8ec4730ead83426dbe89cffc3925789b3f9c14f0
Miracutor <miracleexecutors@gmail.com>
2022-03-26 02:38:43 +0800

    Remove @now

--- src/index.ts
+++ src/index.ts
@@ -165,7 +165,7 @@ export async function build({
 
     const launcherFiles = {
       "now__bridge.js": new FileFsRef({
-        fsPath: require("@now/node-bridge"),
+        fsPath: require("@vercel/node-bridge"),
       }),
       "now__launcher.js": new FileFsRef({
         fsPath: path.join(__dirname, "launcher.js"),
